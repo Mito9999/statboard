797f898b7dbe346f9d35c40e6c5f6cfe644883ee
Mito6211 <willmoore674@gmail.com>
2020-12-14 10:02:45 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { headersArray, API_URL } from "./fetchVariables.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [jsonData, setJsonData] = useState(null);
+
+    const getData = async () => {
+        let myHeaders = new Headers();
+
+        headersArray.forEach((header) => {
+            myHeaders.append(header[0], header[1]);
+        });
+
+        const data = await fetch(API_URL, {
+            method: "POST",
+            headers: myHeaders,
+            redirect: "follow",
+        })
+            .then((response) => response.json())
+            .catch((error) => console.log("error", error));
+
+        const { min_norm, max_norm, avg_norm, languages_sorted } = data;
+        const filtered = { min_norm, max_norm, avg_norm, languages_sorted };
+
+        setJsonData(filtered);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div>{jsonData ? JSON.stringify(jsonData, null, 2) : "Loading..."}</div>
+    );
 }
 
 export default App;
