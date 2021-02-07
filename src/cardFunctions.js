90e5bbccf96f9fd063072504eb22b3f2e54861fb
Mito6211 <willmoore674@gmail.com>
2021-02-07 12:22:24 -0500

    Compressed Constants, modified 10ff headers

--- src/cardFunctions.js
+++ src/cardFunctions.js
@@ -15,15 +15,10 @@ const tenfastfingers = (cardInfo) => {
         ["accept", "application/json, text/javascript, */*; q=0.01"],
         [
             "user-agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Safari/537.36",
         ],
         ["x-requested-with", "XMLHttpRequest"],
         ["origin", "https://10fastfingers.com"],
-        ["sec-fetch-site", "same-origin"],
-        ["sec-fetch-mode", "cors"],
-        ["sec-fetch-dest", "empty"],
-        ["referer", "https://10fastfingers.com/user/"],
-        ["accept-language", "en-US,en;q=0.9"],
     ];
 
     const API_URL = `${CORS_URL}/https://10fastfingers.com/users/get_graph_data/0/`;
--- src/constants.js
+++ src/constants.js
@@ -1,118 +1,28 @@
 export const settingsArray = [
-    {
-        text: "Auto Update",
-        value: "autoUpdate",
-    },
-    {
-        text: "Light Mode",
-        value: "lightMode",
-    },
+    { text: "Auto Update", value: "autoUpdate" },
+    { text: "Light Mode", value: "lightMode" },
 ];
 
 export const searchData = [
-    {
-        name: "Amazon",
-        prefix: "a",
-        url: "https://www.amazon.com/s?k={{query}}",
-    },
-    {
-        name: "Google",
-        prefix: "g",
-        url: "https://www.google.com/search?q={{query}}",
-    },
-    {
-        name: "Github",
-        prefix: "gh",
-        url: "https://github.com/search?q={{query}}",
-    },
-    {
-        name: "Stack Overflow",
-        prefix: "so",
-        url: "https://stackoverflow.com/search?q={{query}}",
-    },
-    {
-        name: "YouTube",
-        prefix: "yt",
-        url: "https://www.youtube.com/results?search_query={{query}}",
-    },
-    {
-        name: "Google Calendar",
-        prefix: "cal",
-        url: "https://calendar.google.com/calendar",
-    },
-    {
-        name: "Google Translate",
-        prefix: "gt",
-        url: "https://translate.google.com/?sl=en&tl=es&text={{query}}",
-    },
-    {
-        name: "Google Fonts",
-        prefix: "gfont",
-        url: "https://fonts.google.com/?query={{query}}",
-    },
-    {
-        name: "React Icons",
-        prefix: "ri",
-        url: "https://react-icons.github.io/react-icons/search?q={{query}}",
-    },
-    {
-        prefix: "w",
-        name: "Wikipedia",
-        url: "https://www.wikipedia.org/wiki/{{query}}",
-    },
-    {
-        prefix: "rtd",
-        name: "Read The Docs",
-        url: "https://readthedocs.org/search/?q={{query}}",
-    },
-    {
-        prefix: "r",
-        name: "Reddit",
-        url: "https://www.reddit.com/search/?q={{query}}",
-    },
-    {
-        prefix: "mdn",
-        name: "Mozilla Developer Network",
-        url: "https://developer.mozilla.org/en-US/search?q={{query}}",
-    },
-    {
-        prefix: "maps",
-        name: "Google Maps",
-        url: "https://www.google.com/maps/search/{{query}}",
-    },
-    {
-        prefix: "yf",
-        name: "Yahoo Finance",
-        url: "https://finance.yahoo.com/quote/{{query}}",
-    },
-    {
-        prefix: "nc",
-        name: "Namecheap",
-        url: "https://www.namecheap.com/domains/registration/results/?domain={{query}}",
-    },
-    {
-        prefix: "nfly",
-        name: "Netlify",
-        url: "https://app.netlify.com/",
-    },
-    {
-        prefix: "sdc",
-        name: "Spanish Dict (Conjugations)",
-        url: "https://www.spanishdict.com/conjugate/{{query}}",
-    },
-    {
-        prefix: "npms",
-        name: "Node Package Manager (NPM) Search",
-        url: "https://www.npmjs.com/search?q={{query}}",
-    },
-    {
-        prefix: "npm",
-        name: "Node Package Manager (NPM) Exact Match",
-        url: "https://www.npmjs.com/package/{{query}}",
-    },
-    {
-        prefix: "vercel",
-        name: "Vercel Dashboard",
-        url: "https://vercel.com/dashboard",
-    },
+    { name: "Amazon", prefix: "a", url: "https://www.amazon.com/s?k={{query}}" },
+    { name: "Google", prefix: "g", url: "https://www.google.com/search?q={{query}}" },
+    { name: "Github", prefix: "gh", url: "https://github.com/search?q={{query}}" },
+    { name: "Stack Overflow", prefix: "so", url: "https://stackoverflow.com/search?q={{query}}" },
+    { name: "YouTube", prefix: "yt", url: "https://www.youtube.com/results?search_query={{query}}" },
+    { name: "Google Calendar", prefix: "cal", url: "https://calendar.google.com/calendar" },
+    { name: "Google Translate", prefix: "gt", url: "https://translate.google.com/?sl=en&tl=es&text={{query}}" },
+    { name: "Google Fonts", prefix: "gfont", url: "https://fonts.google.com/?query={{query}}" },
+    { name: "React Icons", prefix: "ri", url: "https://react-icons.github.io/react-icons/search?q={{query}}" },
+    { name: "Wikipedia", prefix: "w", url: "https://www.wikipedia.org/wiki/{{query}}" },
+    { name: "Read The Docs", prefix: "rtd", url: "https://readthedocs.org/search/?q={{query}}" },
+    { name: "Reddit", prefix: "r", url: "https://www.reddit.com/search/?q={{query}}" },
+    { name: "Mozilla Developer Network", prefix: "mdn", url: "https://developer.mozilla.org/en-US/search?q={{query}}" },
+    { name: "Google Maps", prefix: "maps", url: "https://www.google.com/maps/search/{{query}}" },
+    { name: "Yahoo Finance", prefix: "yf", url: "https://finance.yahoo.com/quote/{{query}}" },
+    { name: "Namecheap", prefix: "nc", url: "https://www.namecheap.com/domains/registration/results/?domain={{query}}" },
+    { name: "Netlify", prefix: "nfly", url: "https://app.netlify.com/" },
+    { name: "Spanish Dict (Conjugations)", prefix: "sdc", url: "https://www.spanishdict.com/conjugate/{{query}}" },
+    { name: "Node Package Manager (NPM) Search", prefix: "npms", url: "https://www.npmjs.com/search?q={{query}}" },
+    { name: "Node Package Manager (NPM) Exact Match", prefix: "npm", url: "https://www.npmjs.com/package/{{query}}" },
+    { name: "Vercel Dashboard", prefix: "vercel", url: "https://vercel.com/dashboard" },
 ];
