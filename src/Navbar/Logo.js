86e45600b2219cb60e6556094431356d35b62636
Mito6211 <willmoore674@gmail.com>
2021-02-11 12:10:31 -0500

    Added logo image to logo component

--- src/Navbar/Logo.js
+++ src/Navbar/Logo.js
@@ -1,7 +1,18 @@
 import React from "react";
+import logo from "./logo.png";
 
 export default function Logo() {
     return (
-        <span style={{ fontSize: "1.5em", fontWeight: "600" }}>Statboard</span>
+        <span
+            style={{
+                fontSize: "1.5em",
+                fontWeight: "600",
+                display: "flex",
+                alignItems: "center",
+            }}
+        >
+            <img src={logo} alt="Statboard Logo" style={{ height: "40px" }} />
+            Statboard
+        </span>
     );
 }
