a9c31a460a033b95d05393f290b4d82cc46238a6
Mito6211 <willmoore674@gmail.com>
2021-02-20 16:41:44 -0500

    Made all Cards.js into stylecomponents

--- src/Cards/Card.js
+++ src/Cards/Card.js
@@ -6,11 +6,81 @@ import MainContext from "../context";
 
 import { MdRefresh, MdClose } from "react-icons/md";
 
+const CardWrapper = styled.div`
+    flex: 1 0 200px;
+    min-width: 250px;
+    height: 250px;
+    margin: 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    border-radius: 25px;
+    -webkit-font-smoothing: subpixel-antialiased;
+    transform: translateZ(0);
+    backface-visibility: hidden;
+    box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.295);
+    transition: all 120ms ease-in;
+    transition-property: transform, box-shadow;
+
+    &:hover {
+        transform: scale(1.03);
+        box-shadow: 5px 8px 15px 0px rgba(0, 0, 0, 0.295);
+    }
+
+    @media (max-width: 500px) {
+        flex-basis: auto;
+        width: 90%;
+    }
+`;
+
+const Title = styled.div`
+    text-align: center;
+    font-size: 1.25rem;
+`;
+
+const Data = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-self: center;
+
+    span {
+        font-size: 2rem;
+        margin-right: 5px;
+    }
+
+    .dollar {
+        font-size: 1.5rem;
+    }
+`;
+
+const Icons = styled.div`
+    margin: 0 auto;
+    font-size: 1.75rem;
+
+    & > * {
+        margin: 0 10px;
+        cursor: pointer;
+    }
+
+    @keyframes spinner {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(359.99deg);
+        }
+    }
+
+    .refreshing {
+        animation: spinner 1s linear infinite;
+    }
+`;
+
 const Card = ({ cardInfo, removeCard, ...restProps }) => {
     const context = useContext(MainContext);
     const [data, setData] = useState(handleEmptyData("Loading..."));
     const [isLoading, setIsLoading] = useState(true);
-    const [hovered, setHovered] = useState(false);
 
     const siteInfo = SITE_INFO[cardInfo.site];
 
@@ -82,39 +152,28 @@ const Card = ({ cardInfo, removeCard, ...restProps }) => {
     }, [context]);
 
     return (
-        <div
-            {...restProps}
-            className={`card ${hovered ? "hovered" : ""}`}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-        >
-            <div className="card--title">{data[0]}</div>
-            <div className="card--data">
+        <CardWrapper {...restProps}>
+            <Title>{data[0]}</Title>
+            <Data>
                 <div>{data[1]}</div>
                 <div>{data[2]}</div>
-            </div>
-            <div
-                className="card--icons"
+            </Data>
+            <Icons
                 style={{
                     color: context.theme.icons,
-                    fontSize: "1.75rem",
                 }}
             >
-                <MdClose
-                    onClick={() => {
-                        removeCard(cardInfo.id);
-                    }}
-                />
+                <MdClose onClick={() => removeCard(cardInfo.id)} />
                 <MdRefresh
                     onClick={() => {
                         if (!isLoading) {
                             getAndSetData();
                         }
                     }}
-                    className={isLoading ? "refresh-spinning" : "refresh"}
+                    className={isLoading ? "refreshing" : ""}
                 />
-            </div>
-        </div>
+            </Icons>
+        </CardWrapper>
     );
 };
 
